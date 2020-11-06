8ff0d864cf8d716018f5c9b7f51c4dbe170d3b2e
Yuta Saito <kateinoigakukun@gmail.com>
2020-11-06 22:50:40 +0900

    Add public API to strip custom section

--- Sources/WasmTransformer/WasmTransformer.swift
+++ Sources/WasmTransformer/WasmTransformer.swift
@@ -5,3 +5,11 @@ public func lowerI64Imports(_ input: [UInt8]) throws -> [UInt8] {
     try transformer.transform(&inputStream, writer: writer)
     return writer.bytes()
 }
+
+public func stripCustomSections(_ input: [UInt8]) throws -> [UInt8] {
+    let transformer = CustomSectionStripper()
+    var inputStream = InputByteStream(bytes: input)
+    let writer = InMemoryOutputWriter()
+    try transformer.transform(&inputStream, writer: writer)
+    return writer.bytes()
+}
